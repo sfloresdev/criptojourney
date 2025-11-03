6a9e8c9656fd57fcfeaba6947b982a3d15455f70
Sergio Flores <sergiofloresperaltarafael@gmail.com>
2025-11-03 17:04:43 +0100

    mejorado footer añadido status, contador y autores. Creado componente de botones

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+// Bloque para el menu
 let btn = document.getElementById('menu-btn');
 let menu = document.getElementById('menu');
 
@@ -10,3 +11,29 @@ document.addEventListener("click", (event) => {
     menu.classList.remove("active");
   }
 });
+
+
+// Bloque para el status de la pagina en el footer
+// Objeto que almacena los posibles estados del sitio
+const projectStates = {
+  active: { text: "Active", className: "active" },
+  maintenance: { text: "In maintenance", className: "maintenance" },
+  offline: { text: "Offline", className: "offline" }
+}
+
+function updateStatus(newStatusKey) {
+  // Elementos del DOM
+  const statusIndicator = document.querySelector('.status-indicator');
+  const statusText = document.querySelector('.status-text');
+
+  // Si los elementos aun no se han cargado
+  if (!statusIndicator || !statusText) return;
+
+  const newState = projectStates[newStatusKey];
+  if (!newState) return; // si no existe, no hace nada
+
+  statusIndicator.className = "status-indicator " + newState.className;
+  statusText.textContent = newState.text;
+}
+
+updateStatus("maintenance");
