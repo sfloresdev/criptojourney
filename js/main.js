b14223d4145e5e5835af2a66bf2b395fc66225ea
Sergio Flores <sergiofloresperaltarafael@gmail.com>
2025-11-05 22:12:29 +0100

    añadido componente de pseudoencriptacion hay que mejorarlo

--- js/main.js
+++ js/main.js
@@ -1,46 +1,19 @@
-// Bloque para el menu
-let btn = document.getElementById('menu-btn');
-let menu = document.getElementById('menu');
+// Animacion de las imagenes al cargar
+window.addEventListener('load', () => {
+  const imgs = document.querySelectorAll('.hero-images img');
 
-btn.addEventListener("click", () => {
-  menu.classList.toggle("active");
+  // Esperar a que el navegador pinte el estado inicial
+  requestAnimationFrame(() => {
+    imgs.forEach((img, i) => {
+      // Añadimos con retraso para que entren una tras otra
+      setTimeout(() => {
+        img.classList.add('visible');
+      }, i * 500);
+    });
+  });
 });
-
-document.addEventListener("click", (event) => {
-  if (!btn.contains(event.target) && !menu.contains(event.target)) {
-    menu.classList.remove("active");
-  }
-});
-
-
-// Bloque para el status de la pagina en el footer
-// Objeto que almacena los posibles estados del sitio
-const projectStates = {
-  active: { text: "Active", className: "active" },
-  maintenance: { text: "In maintenance", className: "maintenance" },
-  offline: { text: "Offline", className: "offline" }
-}
-
-function updateStatus(newStatusKey) {
-  // Elementos del DOM
-  const statusIndicator = document.querySelector('.status-indicator');
-  const statusText = document.querySelector('.status-text');
-
-  // Si los elementos aun no se han cargado
-  if (!statusIndicator || !statusText) return;
-
-  const newState = projectStates[newStatusKey];
-  if (!newState) return; // si no existe, no hace nada
-
-  statusIndicator.className = "status-indicator " + newState.className;
-  statusText.textContent = newState.text;
-}
-
-updateStatus("offline");
-
 
 // Bloque para animacion de fondo en hero Section
-
 const canvas = document.getElementById('heroCanvas');
 const ctx = canvas.getContext('2d');
 
@@ -100,29 +73,63 @@ function animate() {
   ctx.globalAlpha = 1.0;
   requestAnimationFrame(animate);
 }
-
 // Ajustar al redimensionar
 window.addEventListener('resize', () => {
   canvas.width = canvas.offsetWidth;
   canvas.height = canvas.offsetHeight;
 });
-
 // Iniciar
 animate();
 
 
+// Bloque para el menu
+let btn = document.getElementById('menu-btn');
+let menu = document.getElementById('menu');
 
-
-window.addEventListener('load', () => {
-  const imgs = document.querySelectorAll('.hero-images img');
-
-  // 🚀 Esperar a que el navegador pinte el estado inicial
-  requestAnimationFrame(() => {
-    imgs.forEach((img, i) => {
-      // Añadimos con retraso para que entren una tras otra
-      setTimeout(() => {
-        img.classList.add('visible');
-      }, i * 500);
-    });
-  });
+btn.addEventListener("click", () => {
+  menu.classList.toggle("active");
 });
+
+document.addEventListener("click", (event) => {
+  if (!btn.contains(event.target) && !menu.contains(event.target)) {
+    menu.classList.remove("active");
+  }
+});
+
+const encryptionSection = document.querySelector('.encryption-section');
+
+const observer = new IntersectionObserver((entries) =>{
+	entries.forEach((entry)=> {
+		if (entry.isIntersecting) {
+			entry.target.classList.add('visible');
+			observer.unobserve(entry.target);
+		}
+	});
+}, {threshold: 0.5});
+observer.observe(encryptionSection);
+
+
+// Bloque para el status de la pagina en el footer
+// Objeto que almacena los posibles estados del sitio
+const projectStates = {
+  active: { text: "Active", className: "active" },
+  maintenance: { text: "In maintenance", className: "maintenance" },
+  offline: { text: "Offline", className: "offline" }
+}
+
+function updateStatus(newStatusKey) {
+  // Elementos del DOM
+  const statusIndicator = document.querySelector('.status-indicator');
+  const statusText = document.querySelector('.status-text');
+
+  // Si los elementos aun no se han cargado
+  if (!statusIndicator || !statusText) return;
+
+  const newState = projectStates[newStatusKey];
+  if (!newState) return; // si no existe, no hace nada
+
+  statusIndicator.className = "status-indicator " + newState.className;
+  statusText.textContent = newState.text;
+}
+
+updateStatus("offline");
